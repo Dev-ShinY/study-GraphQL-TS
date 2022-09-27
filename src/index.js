29a420a006a0aabac17146890d74639e9873eb9f
shinyunsung <114058540+Dev-ShinY@users.noreply.github.com>
2022-09-27 18:54:18 +0900

    :art: change ApolloClient code using ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import client from "./client";
+import { ApolloProvider } from "@apollo/client"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     {/* Provider는 기본적으로 애플리케이션 안에 모두가 이 client를 접근하게 해줌 */}
+    <ApolloProvider client={ client }>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
